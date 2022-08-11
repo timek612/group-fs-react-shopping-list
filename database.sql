ed77c19010cd93c5550fd1830d91efd5e7d4c65e
AJ Timek <Timek612@gmail.com>
2022-08-11 13:23:16 -0500

    npm install and sql data

--- database.sql
+++ database.sql
@@ -1,2 +1,12 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+CREATE TABLE "items" (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(80) NOT NULL,
+    quantity NUMERIC NOT NULL,
+    unit VARCHAR(20)
+);
+
+INSERT INTO items (name, quantity, unit)
+VALUES ('Milk', '5', 'Gallons'),
+('Bread', '1', 'Loaf')
