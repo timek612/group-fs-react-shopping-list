8e68c3c7a18418123fea26badce53bbd1c90b252
AJ Timek <Timek612@gmail.com>
2022-08-11 13:42:00 -0500

    changed database sql

--- database.sql
+++ database.sql
@@ -4,9 +4,10 @@ CREATE TABLE "items" (
     id SERIAL PRIMARY KEY,
     name VARCHAR(80) NOT NULL,
     quantity NUMERIC NOT NULL,
-    unit VARCHAR(20)
+    unit VARCHAR(20),
+    ispurchased BOOLEAN DEFAULT FALSE
 );
 
 INSERT INTO items (name, quantity, unit)
 VALUES ('Milk', '5', 'Gallons'),
-('Bread', '1', 'Loaf')
+('Bread', '1', 'Loaf');
