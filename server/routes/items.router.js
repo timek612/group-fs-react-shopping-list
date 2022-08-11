15dc938583a750d9de23283b86cdad24c7a9cdf9
Matt Moua <matmoua@gmail.com>
2022-08-11 14:31:45 -0500

    Reset working

--- server/routes/items.router.js
+++ server/routes/items.router.js
@@ -2,7 +2,20 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool.js');
 
-
+router.put('/', (req, res)=>{
+  const sqlText = `
+    UPDATE "items"
+    SET "ispurchased" = FALSE;`;
+  pool.query(sqlText)
+      .then((result) => {
+          console.log('Reset items');
+          res.sendStatus(200);
+      })
+      .catch((error) => {
+          console.log(`Error making database query ${sqlText}`, error);
+          res.sendStatus(500);
+      })
+})
 
 
 module.exports = router;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
-
 import Header from '../Header/Header.jsx'
 import './App.css';
+import axios from 'axios';
 
 
 function App() {
+
+    const handleReset = () => {
+        axios({
+            method: 'PUT',
+            url: '/items'
+        }).then(response=>{
+            console.log(response)
+        }).catch(err=>{
+            console.log(err);
+        })
+    }
+
     return (
         <div className="App">
             <Header />
             <main>
                 <p>Under Construction...</p>
+                <h2>Shopping List</h2>
+                <button onClick={handleReset}>Reset</button> <button>Clear</button>
             </main>
         </div>
     );
